fef9ea9ae2c1b9c973270426b10f28a2996a3025
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-13 20:41:54 +0000

    milestone_2

--- milestone_2.py
+++ milestone_2.py
@@ -4,7 +4,7 @@ word_list = ['apple', 'lemon', 'orange', 'strawberry', 'dragonfruit']
 word = random.choice(word_list)
 print(word)
 guess = input("Enter a single letter ")
-if len(guess) == 1:
+if len(guess) == 1 and guess.isalpha() == True:
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
