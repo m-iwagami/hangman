58b87157904730c338aa4c52b5c8775206f1529e
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-15 01:03:42 +0000

    milestone_3

--- milestone_3.py
+++ milestone_3.py
@@ -1,7 +1,10 @@
 guess = input("Enter a single letter ")
 
-if len(guess) > 1 or guess.isalpha() != True:
-    print("Invalid letter. Please, enter a single alphabetical character.")
+
 while(True):
-   len(guess) == 1 and guess.isalpha() == True
-   break 
+    if len(guess) > 1 or guess.isalpha() != True:
+     print("Invalid letter. Please, enter a single alphabetical character.")
+    else:
+       len(guess) == 1 and guess.isalpha() == True
+       break 
+    
