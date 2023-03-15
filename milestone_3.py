928611b2879f01b5207b32f241b99a21da08505f
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-15 01:25:51 +0000

    milestone3

--- milestone_3.py
+++ milestone_3.py
@@ -1,10 +1,17 @@
+import random
+
+word_list = ['apple', 'lemon', 'orange', 'strawberry', 'dragonfruit']
+word = random.choice(word_list)
 guess = input("Enter a single letter ")
 
-
 while(True):
-    if len(guess) > 1 or guess.isalpha() != True:
-     print("Invalid letter. Please, enter a single alphabetical character.")
-    else:
-       len(guess) == 1 and guess.isalpha() == True
-       break 
+    len(guess) > 1 or guess.isalpha() != True
+    print("Invalid letter. Please, enter a single alphabetical character.")
+    if (len(guess) == 1 or guess.isalpha() == True):
+        break
     
+
+if guess in word:
+    print(f"Good guess {guess} is in the word")
+else:
+    print(f"Sorry,{guess} is not in the word")
