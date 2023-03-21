c8cf55f972f8271fb504a984209c0392663b3500
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-21 09:53:26 +0000

    milestone4 task1

--- milestone_4.py
+++ milestone_4.py
@@ -2,9 +2,9 @@ import random
 
 
 class Hangman:
-    def __init__(self, word_list, num_lives):
+    def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
-        self.num_lives = 5
+        self.num_lives = num_lives
         self.word = list(random.choice(self.word_list).lower)
         self.num_letters = len(set(self.word))
         self.word_guessed = ["_"]*len(self.word)
@@ -14,27 +14,4 @@ class Hangman:
         print(f"{self.word_guessed}")
         pass
 
-
-import random
-
-word_list = ['apple', 'lemon', 'orange', 'strawberry', 'dragonfruit']
-word = random.choice(word_list)
-guess = input("Enter a single letter ")
-
-def check_guess(guess):
-    guess = guess.lower()
-    if guess in word:
-        print(f"Good guess {guess} is in the word")
-    else:
-        print(f"Sorry,{guess} is not in the word")
-
-def ask_for_input():
-    while True:
-        if len(guess) > 1 or guess.isalpha() != True:
-            print("Invalid letter. Please, enter a single alphabetical character.")
-            break
-        elif (len(guess) == 1 and guess.isalpha() == True):
-            check_guess(guess)
-            break
-ask_for_input
 
