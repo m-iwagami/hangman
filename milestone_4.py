ad332f59a39d5de725587a191b318f5b441ff04b
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-21 20:53:12 +0000

    milestone4 task1

--- milestone_4.py
+++ milestone_4.py
@@ -5,6 +5,9 @@ class Hangman:
         self.word_list = word_list
         self.word = list(random.choice(self.word_list).lower)
         self.num_lives = num_lives
-        self.num_letters = len(set(self.word))
-        self.word_guessed = ['_']*len(self.word)
+        self.word_guessed = []
+        for i in xrange(len(self.word)):
+            self.word_guessed.append('-')
+        ['_']*len(self.word)
+        self.num_letters = len(set(self.word)) - self.word_guessed
         self.list_of_guesses = []
