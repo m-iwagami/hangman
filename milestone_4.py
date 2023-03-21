d63eef464750d92fedb806e7065f0774c88be060
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-21 09:50:57 +0000

    milestone4 task1

--- milestone_4.py
+++ milestone_4.py
@@ -1,11 +1,18 @@
+import random
+
+
 class Hangman:
-    def __init__(self, word_list, num_lives=5):
+    def __init__(self, word_list, num_lives):
         self.word_list = word_list
-        self.num_lives = num_lives
-        self.word = word
-        self.num_letters = num_letters
-        self.word_guessed = word_guessed
-        self.list_of_guesses = list_of_guesses
+        self.num_lives = 5
+        self.word = list(random.choice(self.word_list).lower)
+        self.num_letters = len(set(self.word))
+        self.word_guessed = ["_"]*len(self.word)
+        self.list_of_guesses = []
+
+        print(f"The mystery word has:  {len(self.word)} characters")
+        print(f"{self.word_guessed}")
+        pass
 
 
 import random
