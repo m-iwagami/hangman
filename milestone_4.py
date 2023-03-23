627878f8c3d4d291b1241961ad10cf12c44bb716
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-23 16:03:45 +0000

    milestone 4 task 3

--- milestone_4.py
+++ milestone_4.py
@@ -21,6 +21,9 @@ class Hangman:
             self.num_letters -= 1
         else:
             print(f"Sorry,{guess} is not in the word")
+            self.num_lives -= 1
+            print(f"You have {self.num_lives} lives left.")
+
         self.list_of_guesses.append(guess)
 
     def ask_for_input(self):
