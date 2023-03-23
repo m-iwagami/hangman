b937efa9347ca583190c6e8b5587569a46f93f63
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-03-23 15:54:52 +0000

    milestone 4 task 3

--- milestone_4.py
+++ milestone_4.py
@@ -15,10 +15,9 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-            for i, guess in enumerate(self.word):
-               if guess == list(self.word):
+            for i in range(len(self.word)):
+               if self.word[i] == guess:
                     self.word_guessed[i] = guess
-            self.num_letters - 1    
         else:
             print(f"Sorry,{guess} is not in the word")
         self.list_of_guesses.append(guess)
